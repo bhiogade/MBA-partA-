bcd028a1240fa37eeac432323bd88be24ade9193
Sanket Bhiogade <sanket.bhiogade95@gmail.com>
2021-06-08 11:26:43 +0530

    Checkpoint4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './common/header/Header.js'
 import './logo.svg'
-import Home from './screen/Home/Home'
+import Home from './screens/Home/Home'
 
 ReactDOM.render(<Home />,document.getElementById('root'));
 
