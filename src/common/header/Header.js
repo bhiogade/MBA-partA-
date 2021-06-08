823bfa113452898fc40ac49770e1ba5a22c3bf1c
Sanket Bhiogade <sanket.bhiogade95@gmail.com>
2021-06-08 10:14:27 +0530

    Checkpoint1

---
+++ src/common/header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+ render() {
+ return (
+ <div className="header">
+ Phone Directory
+ </div>
+ )
+ }
+}
+export default Header;
